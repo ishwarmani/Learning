00a83b589a03107f86bff36f58702a87517f5939
ishwarmani <ishwarmanithapa@gmail.com>
2018-03-20 11:12:45 +0530

    Using EnumSingleton for DB connection

---
+++ JavaConcepts/src/designpatterns/creational/singleton/EnumSingletonConnection.java
@@ -0,0 +1,34 @@
+package designpatterns.creational.singleton;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public enum EnumSingletonConnection {
+
+    INSTANCE;
+
+    private Connection connection;
+
+    EnumSingletonConnection(){
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sonoo","root","root");
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    // Static getter
+    public static EnumSingletonConnection getInstance() {
+        return INSTANCE;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
+
+/*
+Now you use can use final Singleton s = Singleton.getInstance().
+Remember that since this is an enum you can always access this via Singleton.INSTANCE as well.
+ */
