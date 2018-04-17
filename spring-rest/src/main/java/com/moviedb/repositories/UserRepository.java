354325e46fc77863287539bb2bed351729ebaefd
ishwarmani <ishwarmanithapa@gmail.com>
2018-04-17 14:57:46 +0530

    Dependency added for spring data jpa

--- spring-rest/src/main/java/com/moviedb/repositories/UserRepository.java
+++ spring-rest/src/main/java/com/moviedb/repositories/UserRepository.java
@@ -1,4 +1,8 @@
 package com.moviedb.repositories;
 
-public class UserRepository {
+import com.moviedb.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
